717d6a26c7428c47d0b1f0bfbe30e3e37e2875dd
Ismeet Chapagain <ismeet@Ismeets-MacBook-Pro.local>
2024-10-02 04:37:59 +0545

    Initial commit

---
+++ 1oct.py
@@ -0,0 +1,71 @@
+# x=int(input("Enter a number"))
+# if x>0:
+#     print ("The number is positive")
+# else:
+#     print("The number is negative")
+
+
+# x=int(input("Enter a number:"))
+# if x%2==0:
+#     print("The number is even")
+# else:
+#     print("The number is odd")
+
+
+# x=int(input("Enter a number:"))
+# if x%10==7:
+#     print("The number ends with 7")
+# else:
+#     print("The number does not ends with 7")
+
+
+# x=int(input("enter your first number:"))
+# y=int(input("enter your second number:"))
+# if x>y:
+#     print("The number greater is:",x)
+# else:
+#     print("The number greater is:",y)
+
+
+
+# x=int(input("Enter your first number:"))
+# y=int(input("Enter your second number:"))
+# z=int(input("Enter your third number:"))
+# if x>y and x>z or x>z and x>y:
+#     print("The greatest number is",x)
+# elif y>x and y>z or y>z and y>x:
+#     print ("The greater number is",y)
+# else:
+#     print("The greatest number is",z)
+
+
+# x=int(input("Enter your first number:"))
+# y=int(input("Enter your second number:"))
+# z=int(input("Enter your third number:"))
+# if x<y and x<z or x<z and x<y:
+#     print("The smallest number is",x)
+# elif y<x and y<z or y<z and y<x:
+#     print ("The smallest number is",y)
+# else:
+#     print("The smallest number is",z)
+
+
+# x=int(input("Enter marks of first subject:"))
+# y=int(input("Enter marks of second subject:"))
+# z=int(input("Enter marks of third subject:"))
+# s= x+y+z
+# if s>250:
+#     print("Excellent")
+# elif s<=150:
+#     print("Work harder")
+# else:
+#     print("Good")
+
+
+
+
+
+
+
+
+
